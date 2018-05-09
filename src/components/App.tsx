f6a4a0125389aa4c99a1ff51f9f289adf30dbfe9
Evgenij Beloded <belodedevgenij@gmail.com>
2018-05-09 21:27:56 +0700

    Add styled-components, cleanup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,4 @@
-import { ThemeProvider } from 'emotion-theming'
 import * as React from 'react'
-import styled from 'react-emotion'
 import { Route, Switch } from 'react-router-dom'
 
 import { Log } from '~/utils/log'
@@ -9,29 +7,20 @@ import { Dashboard } from './Dashboard'
 import { Header } from './Header'
 import { MoneyAccountsPage } from './money-accounts/MoneyAccountsPage'
 
-const debug = Log('App:')
-
-const AppLayout = styled('div')({})
-
-const defaultTheme: React.CSSProperties = {
-  backgroundColor: '#333',
-  color: '#ccc',
-}
+const log = Log('App:')
 
 export class App extends React.Component {
   render() {
-    debug('render App')
+    log('render App')
     return (
-      <ThemeProvider theme={defaultTheme}>
-        <AppLayout>
-          <Header />
-          <Switch>
-            <Route path="/categories" component={CategoriesPage} />
-            <Route path="/money-accounts" component={MoneyAccountsPage} />
-            <Route exact={true} path="/" component={Dashboard} />
-          </Switch>
-        </AppLayout>
-      </ThemeProvider>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/categories" component={CategoriesPage} />
+          <Route path="/money-accounts" component={MoneyAccountsPage} />
+          <Route exact={true} path="/" component={Dashboard} />
+        </Switch>
+      </div>
     )
   }
 }
