24dd11c0c21b8d0bc265f2849c878ae05536c44b
Evgenij Beloded <belodedevgenij@gmail.com>
2018-05-10 00:23:49 +0700

    Improve type safety

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,21 @@
-import { FirebaseFirestore, Query, QuerySnapshot, Settings } from '@firebase/firestore-types'
+import {
+  CollectionReference,
+  DocumentData,
+  DocumentReference,
+  DocumentSnapshot,
+  FirebaseFirestore,
+  OrderByDirection,
+  Query,
+  QueryDocumentSnapshot,
+  QuerySnapshot,
+  SetOptions,
+  Settings,
+  SnapshotListenOptions,
+  SnapshotOptions,
+  UpdateData,
+  WhereFilterOp,
+  WriteBatch,
+} from '@firebase/firestore-types'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import { empty, forkJoin, from, Observable, of } from 'rxjs'
@@ -15,30 +32,73 @@ import { Transactions } from './db.transactions'
 
 const log = Log('Database:Init')
 
-declare module '@firebase/firestore-types' {
-  interface SpecialisedDocumentReference<T extends DocumentData | UpdateData> extends DocumentReference {
-    set(data: T, options?: SetOptions): Promise<void>
-    update(data: Partial<T>): Promise<void>
-    update(field: keyof T, value: any, ...moreFieldsAndValues: any[]): Promise<void>
-  }
+// declare module '@firebase/firestore-types' {
+interface $$DocumentReference<T extends DocumentData | UpdateData> extends DocumentReference {
+  set(data: T, options?: SetOptions): Promise<void>
+  update(data: Partial<T>): Promise<void>
+  update(field: keyof T, value: any, ...moreFieldsAndValues: any[]): Promise<void>
+}
 
-  interface SpecializedQuery<T> extends Query {
-    where(fieldPath: keyof T, opStr: WhereFilterOp, value: any): SpecializedQuery<T>
-    orderBy(fieldPath: keyof T, directionStr?: OrderByDirection): SpecializedQuery<T>
-  }
+interface $$DocumentSnapshot<T> extends DocumentSnapshot {
+  ref: $$DocumentReference<T>
+  get(fieldPath: keyof T, options?: SnapshotOptions): any
+}
 
-  interface SpecializedCollectionReference<T> extends CollectionReference {
-    add(data: T): Promise<DocumentReference>
-    doc(documentPath?: string): SpecialisedDocumentReference<T>
-    where(fieldPath: keyof T, opStr: WhereFilterOp, value: any): SpecializedQuery<T>
-    orderBy(fieldPath: keyof T, directionStr?: OrderByDirection): SpecializedQuery<T>
-  }
+interface $$QueryDocumentSnapshot<T extends DocumentData> extends $$DocumentSnapshot<T> {
+  data(options?: SnapshotOptions): T
+}
 
-  export interface FirebaseFirestore {
-    collection(collectionPath: 'categories'): SpecializedCollectionReference<DB.Category>
-    collection(collectionPath: 'transactions'): SpecializedCollectionReference<DB.Transaction>
-    collection(collectionPath: 'moneyAccounts'): SpecializedCollectionReference<DB.MoneyAccount>
-  }
+interface $$QuerySnapshot<T> extends QuerySnapshot {
+  readonly docs: Array<$$QueryDocumentSnapshot<T>>
+}
+
+interface $$Query<T> extends Query {
+  where(fieldPath: keyof T, opStr: WhereFilterOp, value: any): $$Query<T>
+  orderBy(fieldPath: keyof T, directionStr?: OrderByDirection): $$Query<T>
+  onSnapshot(observer: {
+    next?: (snapshot: $$QuerySnapshot<T>) => void
+    error?: (error: Error) => void
+    complete?: () => void
+  }): () => void
+  onSnapshot(
+    options: SnapshotListenOptions,
+    observer: {
+      next?: (snapshot: $$QuerySnapshot<T>) => void
+      error?: (error: Error) => void
+      complete?: () => void
+    },
+  ): () => void
+  onSnapshot(
+    onNext: (snapshot: $$QuerySnapshot<T>) => void,
+    onError?: (error: Error) => void,
+    onCompletion?: () => void,
+  ): () => void
+  onSnapshot(
+    options: SnapshotListenOptions,
+    onNext: (snapshot: $$QuerySnapshot<T>) => void,
+    onError?: (error: Error) => void,
+    onCompletion?: () => void,
+  ): () => void
+}
+
+interface $$CollectionReference<T> extends $$Query<T> {
+  readonly id: string
+  readonly parent: DocumentReference | null
+  readonly path: string
+  isEqual(other: CollectionReference): boolean
+  add(data: T): Promise<$$DocumentReference<T>>
+  doc(documentPath?: string): $$DocumentReference<T>
+}
+interface $$WriteBatch extends WriteBatch {
+  set<T>(documentRef: $$DocumentReference<T>, data: T, options?: SetOptions): $$WriteBatch
+  update<T>(documentRef: $$DocumentReference<T>, data: Partial<T>): $$WriteBatch
+  update<T>(
+    documentRef: $$DocumentReference<T>,
+    field: keyof T,
+    value: any,
+    ...moreFieldsAndValues: any[]
+  ): $$WriteBatch
+  delete(documentRef: DocumentReference): $$WriteBatch
 }
 
 const firestoreFacade = (firestore: FirebaseFirestore) => ({
@@ -46,15 +106,15 @@ const firestoreFacade = (firestore: FirebaseFirestore) => ({
 })
 
 export class FirestoreFacade {
-  categories = this.firestore.collection('categories')
-  transactions = this.firestore.collection('transactions')
-  moneyAccounts = this.firestore.collection('moneyAccounts')
+  categories = this.firestore.collection('categories') as $$CollectionReference<DB.Category>
+  transactions = this.firestore.collection('transactions') as $$CollectionReference<DB.Transaction>
+  moneyAccounts = this.firestore.collection('moneyAccounts') as $$CollectionReference<DB.MoneyAccount>
   constructor(private firestore: FirebaseFirestore) {}
 
-  batch = () => this.firestore.batch()
-  createSnapshotObservable = (query: Query) => new Observable<QuerySnapshot>(query.onSnapshot.bind(query))
-  querySnapshotToDocumentArray = <T>(querySnapshot: QuerySnapshot): T[] =>
-    querySnapshot.docs.map((queryDocumentSnapshot) => queryDocumentSnapshot.data() as T)
+  batch = () => this.firestore.batch() as $$WriteBatch
+  createSnapshotObservable = <T>(query: $$Query<T>) => new Observable<$$QuerySnapshot<T>>(query.onSnapshot.bind(query))
+  querySnapshotToDocumentArray = <T>(querySnapshot: $$QuerySnapshot<T>) =>
+    querySnapshot.docs.map((queryDocumentSnapshot) => queryDocumentSnapshot.data())
 }
 
 interface DatabaseSettings {
