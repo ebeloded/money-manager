a053baa8fef790c3db35837beade98ea36d28763
Evgenij Beloded <belodedevgenij@gmail.com>
2018-05-10 21:19:52 +0700

    Add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { DatabaseProvider } from '~/db/react-db/DatabaseContext'
 import { App } from './components/App'
 import { initFirebase } from './firebase/initFirebase'
 
+import { css, injectGlobal } from 'emotion'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
@@ -13,7 +14,6 @@ const firebaseApp = initFirebase()
 const db = new Database(firebaseApp)
 
 // import registerServiceWorker from './registerServiceWorker';
-
 ReactDOM.render(
   <DatabaseProvider db={db}>
     <BrowserRouter>
@@ -23,4 +23,20 @@ ReactDOM.render(
   document.getElementById('root') as HTMLElement,
 )
 
+const global = css`
+  body {
+    margin: 0;
+    font-size: 14px;
+  }
+
+  html {
+    background: #333;
+    color: #fff;
+    font-family: 'Fira Mono', 'Roboto Mono';
+  }
+`
+// tslint:disable-next-line:no-unused-expression
+injectGlobal`
+  ${global}
+`
 // registerServiceWorker();
